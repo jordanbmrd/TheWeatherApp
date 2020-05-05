89dc7fdfdccedf4d3cd88e93c7475c89c37a62f3
NotSpread <jordanbmrd@gmail.com>
2020-05-05 21:35:01 +0200

    Initial Commit

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Jordan Baumard on 05/05/2020.
+//  Copyright © 2020 Jordan Baumard. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
